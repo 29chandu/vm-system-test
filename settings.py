36ec1c6c03c5abc179809fea72bb70595b8d6b21
Chandra <scsalutagi@gmail.com>
2022-11-30 16:50:16 +0530

    Test commit

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ if local_run is True:
 class Config:
 
     # SSH server info
-    SSH_HOST = os.getenv('SSH_HOST')
-    SSH_PORT = os.getenv('SSH_PORT')
-    SSH_USER = os.getenv('SSH_USER')
-    SSH_PASSWORD = os.getenv('SSH_PASSWORD')
+    SSH_HOST = os.getenv('SSH_HOST') or 'localhost'
+    SSH_PORT = os.getenv('SSH_PORT') or '2222'
+    SSH_USER = os.getenv('SSH_USER') or 'chandra'
+    SSH_PASSWORD = os.getenv('SSH_PASSWORD') or 'Kuppi@298'
