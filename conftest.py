e0677597d60655aa5b6358430beaa2559d62e5fd
Chandra <scsalutagi@gmail.com>
2022-12-01 12:36:10 +0530

    debug commit

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from paramiko import SSHClient
+from paramiko import SSHClient, AutoAddPolicy
 from settings import Config
 
 
@@ -10,6 +10,7 @@ def ssh_client():
 
     client = SSHClient()
     client.load_system_host_keys()
+    client.set_missing_host_key_policy(AutoAddPolicy())
     client.connect(hostname=Config.SSH_HOST, port=Config.SSH_PORT,
                    username=Config.SSH_USER, password=Config.SSH_PASSWORD)
     return client
