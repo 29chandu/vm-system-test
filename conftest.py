1a91f563a8d8d3d1d4c5a4ac17044b7467120651
Chandra <scsalutagi@gmail.com>
2022-12-02 10:04:25 +0530

    test commit

--- conftest.py
+++ conftest.py
@@ -6,7 +6,8 @@ from settings import Config
 @pytest.fixture(autouse=True)
 def ssh_client():
     print(
-        f'\n\n\n\nDebug SSH info: host={Config.SSH_HOST}, port={Config.SSH_PORT}, user={Config.SSH_USER}, pw={Config.SSH_PASSWORD}\n\n\n\n\n')
+        f'\n\n\n\nDebug SSH info: host={Config.SSH_HOST},',
+        f' port={Config.SSH_PORT}, user={Config.SSH_USER}, pw={Config.SSH_PASSWORD}\n\n\n\n\n')
 
     client = SSHClient()
     client.load_system_host_keys()
