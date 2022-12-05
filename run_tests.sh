bc2c43fb3df926d0cc093a56c4c38eb4cece32fc
Chandra <scsalutagi@gmail.com>
2022-12-05 17:03:21 +0530

    allure report generation

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 python3.10 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
-python -m pytest --html=report.html --self-contained-html
+python -m pytest --alluredir=/home/chandra/chandra/jenkins/allure-report --html=report.html --self-contained-html
