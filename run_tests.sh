5abbe71551ad78e11c42f90e4b3bc8cf7af799f7
Chandra <scsalutagi@gmail.com>
2022-12-05 16:22:22 +0530

    Add pytest-html report

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 python3.10 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
-python -m --html=report.html --self-contained-html
+python -m pytest --html=report.html --self-contained-html
